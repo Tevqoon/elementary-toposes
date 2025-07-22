2a86fafe5bfe0a505fff68d58e0980e0838f8bc4
Jure Smolar <jure.smolar@gmail.com>
2025-07-22 17:27:25 +0200

    Basic imports

--- ElementaryToposes.lean
+++ ElementaryToposes.lean
@@ -1,3 +1,6 @@
 -- This module serves as the root of the `ElementaryToposes` library.
 -- Import modules here that should be built as part of the library.
 import ElementaryToposes.Basic
+import ElementaryToposes.ChosenFiniteLimits
+import ElementaryToposes.SubobjectClassifier
+import ElementaryToposes.Topos
